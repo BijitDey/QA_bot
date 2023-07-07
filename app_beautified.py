e903f8dfde655e3a8d71ef8a132ebb6a962b8a40
bijit <bijitdey@affine.ai>
2023-07-07 10:30:51 +0530

    back up

--- app_beautified.py
+++ app_beautified.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from databricks_api import DatabricksAPI
-import requests, json, tempfile, os
+import requests, json, tempfile, os, time
 
 # getting environment variables
 from dotenv import load_dotenv
@@ -8,7 +8,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 st.set_page_config(layout="wide")
-st.markdown("<h1 style='text-align: center; font-size: 5em; color: orange;'>Q&A Bot using Dolly</h1>", unsafe_allow_html=True)
+st.markdown("""
+    <div style='text-align: center;'>
+        <h2 style='font-size: 70px; font-family: Arial, sans-serif; 
+                   letter-spacing: 2px; text-decoration: none;'>
+            <a href='https://affine.ai/' target='_blank' rel='noopener noreferrer'
+               style='background: linear-gradient(45deg, #ed4965, #c05aaf);
+                      -webkit-background-clip: text;
+                      -webkit-text-fill-color: transparent;
+                      text-shadow: none; text-decoration: none;'>
+                AffineChat
+            </a>
+        </h2>
+    </div>
+""", unsafe_allow_html=True)
+
 col1, _, col2 = st.columns([1, 0.5, 2])
 
 DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
@@ -16,51 +30,77 @@ DATABRICKS_TOKEN =os.getenv("DATABRICKS_TOKEN")
 API_endpoint = os.getenv("API_endpoint")
 # print(DATABRICKS_HOST)
 # print(DATABRICKS_TOKEN)
+# print(API_endpoint)
 
+
+db = DatabricksAPI(host=DATABRICKS_HOST, token=DATABRICKS_TOKEN)
 
 def res(query=str):
     response = requests.request(method='POST',
                                 headers={'Authorization': f'Bearer {DATABRICKS_TOKEN}'},
                                 url=API_endpoint,
                                 data=query)
-    print(response.content)
     return response.content
 
 def main():
-    col1.header("Upload PDF Text")
-    col2.header("Ask your PDF 💬")
+    col1.header("Upload your File ")
+    col2.header("Ask your File 💬")
 
-    pdf = col1.file_uploader("Upload your PDF") #, type=["pdf"])
+    file = col1.file_uploader("Supports PDF, MP3, WAV, MP4") #, type=["pdf,mp3,mpeg"])
 
-    if pdf is not None:
+    if file is not None:
         if col1.button(f"Upload"):
-            file_details = {"FileName": pdf.name, "FileType": pdf.type}
+            file_details = {"FileName": file.name, "FileType": file.type}
             # st.write(file_details)
 
             with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                temp_file.write(pdf.read())
+                temp_file.write(file.read())
                 file_path = temp_file.name
 
             # st.write("File Path:", file_path)
 
-            db = DatabricksAPI(host=DATABRICKS_HOST, token=DATABRICKS_TOKEN)
             progress_text = col1.empty()
-            progress_text.text("File is being indexed...")
-            db.dbfs.put(path=f"dbfs:/data/{pdf.name}",
+            progress_text.text("File is being uploaded...")
+            db.dbfs.put(path=f"dbfs:/FileStore/data/{file.name}",
                         src_path=file_path,
                         overwrite=True)
+            progress_text.text("File is being indexed...")
             os.remove(file_path)
             progress_text.empty()
             prog_text = col1.empty()
-            prog_text.text(f"{pdf.name} \n Indexed Successfully")
+            # progress_text.text("Indexing in progress...")
+            # print(file.type)
+            if file.type!="application/pdf":
+                for _ in range(30): 
+                    time.sleep(0.5) 
+                    progress_text.text("Indexing in progress" + "." * (_ % 4))
+            prog_text.text(f"{file.name} \n Indexed Successfully")
 
-    user_question = col2.text_input("Ask a question about your PDF:")
+    user_question = col2.text_input("Ask a question based on the content of your file:")
     # if col2.button(f"Submit_{hash('query')}"):
     if col2.button(f"Submit query"):
         if user_question:
             response = res(user_question)
-            generated_text = json.loads(response)
-            col2.write(generated_text)
+            try:
+                generated_text = json.loads(response)
+                output_text = generated_text["output_text"]
+                similar_docs = generated_text["similar_docs"]
+
+                # Display output_text
+                col2.subheader("Answer:")
+                col2.write(output_text)
+
+                # Display similar_docs
+                col2.subheader("Context from the uploaded file:")
+                for doc in similar_docs:
+                    col2.write(f"Page: {doc['metadata']['page']}")
+                    col2.write(doc['page_content'])
+                    col2.write("---")
+
+            except ValueError:
+                col2.write("Error: Invalid response format.")
+            except KeyError:
+                col2.write("Error: Missing required fields in the response.")
 
 if __name__ == '__main__':
     main()
